a9a30d56b6ca70eb77da361d7d35750807097bd7
Andrew Moon <liquidsun@gmail.com>
2014-05-18 19:12:26 -0500

    detect stdint, and detect int types if stdint is missing

--- driver/driver.h
+++ driver/driver.h
@@ -2,7 +2,20 @@
 #define DRIVER_H
 
 #include <stddef.h>
-#include <stdint.h> /* maybe do this more portably later */
 #include "config.h"
+
+/* use HAVE_INTxxx to determine if an integer with [8,16,32,64,128] bits is available */
+
+#if defined(HAVE_STDINT)
+	#include <stdint.h>
+#else
+	DEFINE_INT64
+	DEFINE_INT32
+	DEFINE_INT16
+	DEFINE_INT8
+#endif
+
+/* stdint doesn't currently detect this, but may in the future */
+DEFINE_INT128
 
 #endif /* DRIVER_H */
