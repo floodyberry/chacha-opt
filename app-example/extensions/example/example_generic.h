3f9675763d70bdc9606f67d7e45a17675203da23
Andrew Moon <liquidsun@gmail.com>
2014-08-19 16:02:06 -0500

    remove non-framework files

--- app-example/extensions/example/example_generic.h
+++
@@ -1,8 +0,0 @@
-static unsigned char
-example_generic(const unsigned char *arr, size_t count) {
-	size_t i;
-	unsigned char sum = 0;
-	for (i = 0; i < count; i++)
-		sum += arr[i];
-	return (sum % 256);
-}
--- app-example/extensions/secure_zero/secure_zero_generic.h
+++
@@ -1,7 +0,0 @@
-static void * (*volatile memset_volatile)(void *, int, size_t) = memset;
-
-static void
-secure_zero_generic(unsigned char *p, size_t len) {
-	memset_volatile(p, 0, len);
-}
-
